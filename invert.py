9ddb5765e61e0edbeca062084fb1f588755ee859
Vinicius <viniciiussilva97@outlook.com>
2024-03-18 21:33:26 -0300

    Initial Commit

---
+++ invert.py
@@ -0,0 +1,2 @@
+string = input("Digite uma Frase: ")
+print(string[::-1])
---
+++ logicaNumerica.py
@@ -0,0 +1,22 @@
+'''A primeira sequencia é
+a) 1, 3, 5, 7, 11, onde a lógica seria a sequencia de números primos
+
+A segunda sequencia é
+b) 2, 4, 8, 16, 32, 64, 128, onde a lógica seria a sequencia segue o dobro do numero anterior sendo 128
+
+A terceira sequencia é
+c) 0, 1, 4, 9, 16, 25, 36, 49 onde a lógica seria a sequencia segue o quadrado dos numeros naturais e o proximo seria o 7 = 49;
+
+A quarta sequencia é
+d) 4, 16, 36, 64,  onde a lógica é em ordem crescente o quadrado dos numeros pares sendo o proximo o número 13;
+
+A quinta sequencia é
+e) 1, 1, 2, 3, 5, 8, 13 seria a sequencia de fibonacci e o proximo numero é o 13;
+
+A sexta sequencia é
+b) 2, 10, 12, 16, 17, 18, 19, onde o 200 seria o proximo por ter apenas numeros começados com a letra D
+
+
+
+
+'''
